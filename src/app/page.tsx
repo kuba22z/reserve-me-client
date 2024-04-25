95ea25d0b6d04a692ab38aac8b89e0570e50a597
kuba22z <kuba6330@gmail.com>
2024-04-25 18:53:33 +0200

    implement: fetch token from server, call authorized endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import {Suspense} from "react";
 
-export default function Home() {
+function MissingAuthorizationCodeFallback() {
+  return <>Fail</>
+}
+
+type TokenResponse = {id_token: string, access_token: string,refresh_token: string,expires_in: number,token_type: string}
+
+const getToken = async (authorizationCode: string): Promise<TokenResponse> =>  await fetch('http://localhost:3000/auth',{method: "POST",   headers: {
+    'Content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  },body: JSON.stringify({code: authorizationCode})}).then((response) => response.json())
+    .then((data) => {
+      console.log(data.id_token);
+      return data
+    })
+    .catch((error) => {
+      console.error(error);
+      throw new Error('Failed to fetch data')
+    });
+
+const getAllClients = async (token:string): Promise<string> =>  await fetch('http://localhost:3000/client',{method: "GET",   headers: {
+        'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': 'Bearer ' + token
+    }}).then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        return data.message
+    })
+    .catch((error) => {
+        throw new Error('Failed to fetch data')
+    });
+
+export default async function Home({searchParams}: {
+  searchParams: { code: string | undefined };
+}) {
+  const data =searchParams.code ? await getToken(searchParams.code) : null
+    const client =data ? await getAllClients(data.access_token) : null
+
   return (
+      <Suspense fallback={<MissingAuthorizationCodeFallback />}>
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      It works
     </main>
+      </Suspense>
+
   );
 }
